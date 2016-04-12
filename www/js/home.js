3665090e1a91f2635d1690a8d2301e7f17495e84
Jonatán López <jonatan_angelo@hotmail.com>
2016-04-12 05:08:22 -0500

    8

--- www/js/home.js
+++ www/js/home.js
@@ -2,13 +2,26 @@ var Home = function(){
 
 	facebookConnectPlugin.getLoginStatus(function(obj){
 		console.log(obj);
+		if(obj.status == "connected"){
+			facebookConnectPlugin.api("/me?fields=email,first_name,last_name,gender", null, function(res){
+				console.log(res);
+			}, function(e){
+				console.log(e);
+			});
+		}
 	}, function(err){
 		console.log(err);
 	});
 
 	new Boton($("#home .bt.fb"),function(){
 		facebookConnectPlugin.login(["public_profile", "email"],function(obj){
-			console.log(obj);
+			if(obj.status == "connected"){
+				facebookConnectPlugin.api("/me?fields=email,first_name,last_name,gender", null, function(res){
+					console.log(res);
+				}, function(e){
+					console.log(e);
+				});
+			}
 		}, function(err){
 			console.log(err);
 		})
