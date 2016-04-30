84f32719cd1176be6904dac71c69e92881f281b4
Jonatán López <jonatan_angelo@hotmail.com>
2016-04-29 23:26:09 -0500

    nueva version

--- www/js/ubicacion.js
+++ www/js/ubicacion.js
@@ -10,7 +10,7 @@ var Ubicacion = function(){
 		if(!existemapa){
 			existemapa=true;
 
-			var styles = [
+			/*var styles = [
 		      {
 		        featureType:"poi.business",
 		        elementType:"labels",
@@ -25,13 +25,41 @@ var Ubicacion = function(){
 		    var mapOptions = {
 		      center: new google.maps.LatLng(-12.071854, -77.115097),
 		      zoom: 10,
-		      mapTypeId: 'roadmap',
+		      mapTypeId: 'OSM',
 		      streetViewControl:false,
 		      mapTypeControl:false,
 		      zoomControl: false,
 		      styles: styles
 		    };
+
+
 		    mapa = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		    mapa.mapTypes.set("OSM", new google.maps.ImageMapType({
+                getTileUrl: function(coord, zoom) {
+                    return "http://tile.openstreetmap.org/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+                },
+                tileSize: new google.maps.Size(256, 256),
+                name: "OpenStreetMap",
+                maxZoom: 18
+            }));*/
+
+
+			//var db = window.sqlitePlugin.openDatabase({name: "limamapa.sqlite"});
+			mapa = L.map('map-canvas').setView([-12.050987, -77.112587], 12);
+		
+			//this works, but is online:
+			/*
+			L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				maxZoom: 18
+			}).addTo(map);
+			*/
+			
+			//TODO build something to fall back to web if not found.
+			L.tileLayer('MapQuest/{z}/{x}/{y}.jpg', {
+				maxZoom: 16,
+				minZoom: 12
+			}).addTo(mapa);
 		}
 		var idsMiembros = new Array();
 		$.each(internagrupo.miembros,function(key,val){
@@ -62,6 +90,7 @@ var Ubicacion = function(){
 	
 
 	this.onPosiciones = function(data){
+		
 		console.log(data);
 		console.log(internagrupo.miembros);
 		$.each(internagrupo.miembros,function(key,val){
@@ -70,7 +99,7 @@ var Ubicacion = function(){
 					//crear marker
 					var img = escape(val.pic);
 
-					var icono = {
+					/*var icono = {
 				    	url:'http://picnic.pe/clientes/lifesignal/api/imagen.php?path='+img,
 				    	scaledSize: new google.maps.Size(40, 40)
 				    }
@@ -83,19 +112,46 @@ var Ubicacion = function(){
 				      zIndex:999,
 				      map:mapa,
 				      position:{lat:data[parseInt(val.id)].lat,lng:data[parseInt(val.id)].lon}
-				    });
+				    });*/
+					var lat = data[parseInt(val.id)].lat;
+					var lng = data[parseInt(val.id)].lon;
+
+
+					var myIcon = L.icon({
+					    iconUrl: 'http://picnic.pe/clientes/lifesignal/api/imagen.php?path='+img,
+					    //iconRetinaUrl: 'my-icon@2x.png',
+					    iconSize: [40, 40],
+					    //iconAnchor: [20, 20],
+					    //popupAnchor: [-3, -76],
+					    //shadowUrl: 'my-icon-shadow.png',
+					    //shadowRetinaUrl: 'my-icon-shadow@2x.png',
+					    //shadowSize: [68, 95],
+					    //shadowAnchor: [22, 94]
+					});
+
+					var marker = L.marker([lat,lng],{
+						icon:myIcon
+					}).addTo(mapa);
+
 				   
 				    ubicacion.markers[parseInt(val.id)] = marker;
 				}else{
+					
 					var m = ubicacion.markers[parseInt(val.id)];
 
-					var latlng = new google.maps.LatLng(data[parseInt(val.id)].lat,data[parseInt(val.id)].lon);
-					m.setPosition(latlng);
+					//var latlng = new google.maps.LatLng(data[parseInt(val.id)].lat,data[parseInt(val.id)].lon);
+					//m.setPosition(latlng);
+
+					m.setLatLng([data[parseInt(val.id)].lat,data[parseInt(val.id)].lon]);
+
 				}
 				
-				google.maps.event.clearInstanceListeners(ubicacion.markers[parseInt(val.id)]);
+				//google.maps.event.clearInstanceListeners(ubicacion.markers[parseInt(val.id)]);
 
-				ubicacion.markers[parseInt(val.id)].addListener("click",function(){
+				//ubicacion.markers[parseInt(val.id)].addListener("click",function(){
+				ubicacion.markers[parseInt(val.id)].on("click",function(e){
+
+
 					var fecha = new Date(data[parseInt(val.id)].datetime);
 					console.log(fecha.getHours());
 
@@ -115,6 +171,8 @@ var Ubicacion = function(){
 					var dia = String("0"+String(fecha.getDate())).substr(-2);
 					var mes = String("0"+String(fecha.getMonth()+1)).substr(-2);
 					var anio = fecha.getFullYear();
+
+					
 
 					var formatdate = hora+":"+minutos+ampm+" "+dia+"/"+mes+"/"+anio;
 
