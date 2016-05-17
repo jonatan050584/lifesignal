c7ee09d3594a23b9658e6b09d020003e2627b962
Jonatán López <jonatan_angelo@hotmail.com>
2016-05-17 18:37:24 -0500

    va

--- www/js/header.js
+++ www/js/header.js
@@ -77,12 +77,26 @@ var Header = function(){
 				console.log("aca");
 				console.log(navigator);
 				console.log(navigator.contacts);
-				navigator.contacts.pickContact(function(contact){
+				/*navigator.contacts.pickContact(function(contact){
 			        console.log('The following contact has been selected:' + JSON.stringify(contact));
 			        //internagrupo.seleccionarContacto(contact);
 			    },function(err){
 			        console.log('Error: ' + err);
-			    });
+			    });*/
+
+				navigator.contactsPhoneNumbers.list(function(contacts) {
+			      console.log(contacts.length + ' contacts found');
+			      for(var i = 0; i < contacts.length; i++) {
+			         console.log(contacts[i].id + " - " + contacts[i].displayName);
+			         for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+			            var phone = contacts[i].phoneNumbers[j];
+			            console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
+			         }
+			      }
+			   }, function(error) {
+			      console.error(error);
+			   });
+							
 			}else{
 				$.ajax({
 					url:'contacto.json',
